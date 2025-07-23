181a05586d7c70865f3a7616a83a33ef6ea941c3
AbhishekTripathi28 <abhishek.12028@gmail.com>
2025-07-24 02:27:09 +0530

    setup clerk

--- app/page.js
+++ app/page.js
@@ -4,6 +4,13 @@ import Image from "next/image";
 export default function Home() {
   return (
     <>
+
+    {/* thwre 0is 3 componete 
+    1- hero 
+    2 - features
+    
+    3
+     */}
     <Button>click me</Button>
     </>
   );

2b80ff39ad550c7d1569104c49cb5a38d4e4a775
AbhishekTripathi28 <abhishek.12028@gmail.com>
2025-07-25 01:48:45 +0530

    ui completed

--- app/layout.js
+++ app/layout.js
@@ -23,7 +23,7 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={`${geistSans.variable} ${geistMono.variable} antialiased bg-[#FAF9F6]`}
       >
          <ClerkProvider
           publishableKey={process.env.NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY}
--- components/Header.jsx
+++ components/Header.jsx
@@ -14,31 +14,33 @@ import { Authenticated, Unauthenticated } from "convex/react";
 export default function Header() {
   const { isLoading } = useStoreUser();
   const path = usePathname();
+  
 
   return (
     <header className="fixed top-0 w-full border-b bg-white/95 backdrop-blur z-50 supports-[backdrop-filter]:bg-white/60">
-      <nav className="container mx-auto px-4 h-16 flex items-center justify-between">
+      <nav className="containe mx-auto px-4 h-16 flex items-center justify-between">
         <Link href="/" className="flex items-center gap-2">
           <Image
-            src={"/logos/splitlytic_vertical.png"}
+            src={"/logos/splitlytic_logo_crop.PNG"}
             alt="splitlytic Logo"
-            width={500}
-            height={60}
+            width={700}
+            height={100}
             className="h-11 w-auto object-contain"
           />
         </Link>
 
         {path === "/" && (
-          <div className="hidden md:flex items-center gap-6">
+          <div className="hidden md:flex items-center justify-center gap-6">
             <Link
               href="#features"
-              className="text-sm font-medium hover:text-green-600 transition"
+              className="text-lg font-medium hover:text-green-600 transition"
             >
               Features
             </Link>
             <Link
               href="#how-it-works"
-              className="text-sm font-medium hover:text-green-600 transition"
+              className="text-lg font-medium hover:text-green-600 transition"
+              
             >
               How It Works
             </Link>
@@ -52,7 +54,7 @@ export default function Header() {
                 variant="outline"
                 className="hidden md:inline-flex items-center gap-2 hover:text-green-600 hover:border-green-600 transition"
               >
-                <LayoutDashboard className="h-4 w-4" />
+                <LayoutDashboard className="h-4 w-4 text-base" />
                 Dashboard
               </Button>
               <Button variant="ghost" className="md:hidden w-10 h-10 p-0">
